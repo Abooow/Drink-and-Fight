2b2f333941a7fa3776eab879026663f43ff4363e
Timothy <61967476+timothytin@users.noreply.github.com>
2020-05-27 14:04:43 +0200

    CharacterMover klassen påbörjad

---
+++ Fight-and-Drink/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+// A class that holds values related to the players movement
+public class CharacterMover : MonoBehaviour
+{
+    
+    public float MaxStamina { get; set; }
+    public float CurrentStamina { get; set; }
+    public float Speed { get; set; }
+    public enum MovingState { }
+    public float LimpingMultiplier { get; set; }
+    public float WalkingMultiplier { get; set; }
+    public float SprintingMultiplier { get; set; }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //Returnerar speed
+    public float GetSpeed()
+    {
+        return this.Speed;
+    }
+}
+
+enum MovingState
+{
+    Standing,
+    Limping,
+    Walking,
+    Running
+}
---
+++ Fight-and-Drink/Assets/Scripts/WorldConstants.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldConstants 
+{
+    const float CharacterSpeed = 10;
+
+    const float MovingFireSpeed = 0.95f;
+    
+}
