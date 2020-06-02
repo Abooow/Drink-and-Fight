7f03dc36bbf5efbdfbac06fa1e7f06b2949dfc85
Timothy <61967476+timothytin@users.noreply.github.com>
2020-06-02 16:28:20 +0200

    Player movement

---
+++ Fight-and-Drink/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Controls the Players movement.
+/// </summary>
+public class PlayerMovement : CharacterMover
+{
+    private Rigidbody2D rb2d;
+
+    /// <summary>
+    /// Start is called before the first frame update.
+    /// </summary>
+    protected override void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    /// <summary>
+    /// Update is called once per frame.
+    /// </summary>
+    protected override void Update()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        transform.rotation = Quaternion.LookRotation(transform.position - mousePosition, Vector3.forward);
+        transform.eulerAngles = new Vector3(0, 0, transform.eulerAngles.z);
+
+        Vector2 moveDirection = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+
+        rb2d.AddForce(moveDirection * GetSpeed() * Time.deltaTime);
+    }
+
+    public override float GetSpeed()
+    {
+
+        switch (this.MovingState)
+        {
+            case MovingState.Limping:
+                return this.Speed * this.LimpingMultiplier;
+            case MovingState.Walking:
+                return this.Speed * this.WalkingMultiplier;
+            case MovingState.Running:
+                return this.Speed * this.RunningMultiplier;
+            default:
+                return 0;
+        }
+    }
+}
---
+++ Fight-and-Drink/Assets/Scripts/WorldConstants.cs
@@ -0,0 +1,7 @@
+﻿
+public static class WorldConstants
+{
+    public const float CharacterSpeed = 100;
+
+    public const float MovingFireSpeed = 0.95f;
+}
