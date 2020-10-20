5988ab5a432313c4e96f5717ce666e85f7535763
unknown <eveleddie00@gmail.com>
2020-10-20 14:32:10 +0200

    Made the car and made the movment basics

---
+++ Fight-and-Drink/Assets/Scripts/VehicleMovment.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VehicleMovment : MonoBehaviour
+{
+    public float MaxSpeed;
+    public float Acceleration;
+    public Rigidbody2D car; 
+
+    void Update()
+    {
+        if(Input.GetKey("w"))
+        {
+            Accelerate();
+        }
+    }
+    public void Accelerate()
+    {
+        car.AddForce(transform.up*Acceleration);
+        Debug.Log("BRUMM");
+    }
+
+}
