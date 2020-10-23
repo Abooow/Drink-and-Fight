be94127ae252293d96771418eb61db3c023d9828
Timothy <timothy.jonsson5@gmail.com>
2020-10-23 10:19:24 +0200

    path ai

---
+++ Fight-and-Drink/Assets/Scripts/Patrol.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+    public Transform[] moveSpots;
+    public int moveSpot;
+    private int randomSpot;
+
+    void Start()
+    {
+        waitTime = startWaitTime;
+        moveSpot = 0;
+        randomSpot = Random.Range(0, moveSpots.Length);
+    }
+
+    void Update()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[moveSpot].position, speed*Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, moveSpots[moveSpot].position) < 0.2f)
+        {
+            if(waitTime <= 0){
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+
+                moveSpot += 1;
+                if(moveSpot >= moveSpots.Length)
+                {
+                    moveSpot = 0;
+                }
+
+            }else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+}
